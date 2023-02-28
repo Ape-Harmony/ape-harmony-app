0db4807efc0eeabdd8779334f097348578a89b06
Alex <alexastrum@gmail.com>
2023-02-27 21:48:01 -0700

    Add Alchemy

--- packages/nextjs/components/SwitchTheme.tsx
+++ packages/nextjs/components/SwitchTheme.tsx
@@ -3,7 +3,7 @@ import { useDarkMode, useIsMounted } from "usehooks-ts";
 import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
 
 const SwitchTheme = ({ className }: { className?: string }) => {
-  const { isDarkMode, toggle } = useDarkMode(false);
+  const { isDarkMode, toggle } = useDarkMode(true);
   const isMounted = useIsMounted();
 
   useEffect(() => {
--- packages/nextjs/pages/my-nfts.tsx
+++ packages/nextjs/pages/my-nfts.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import NftGallery from "~~/components/alchemy/NftGallery";
 import { ContractData, ContractInteraction } from "~~/components/ExampleUi";
 
 const ExampleUI: NextPage = () => {
@@ -15,6 +16,7 @@ const ExampleUI: NextPage = () => {
         <br />
         TODO: NFTs in my wallet, order desc by maket value
       </div>
+      <NftGallery walletAddress={"vitalik.eth"} chain={"ETH_MAINNET"} />
     </>
   );
 };
