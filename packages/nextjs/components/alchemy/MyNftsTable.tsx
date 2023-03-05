f3dce7ea938d4ce6cbb7c53a174b3a7f32cbb07f
Alex <alexastrum@gmail.com>
2023-03-04 20:01:17 -0700

    Load my nfts from Mumbai

--- packages/nextjs/components/alchemy/MyNftsTable.tsx
+++ packages/nextjs/components/alchemy/MyNftsTable.tsx
@@ -1,24 +1,14 @@
 import styles from "../../styles/NftGallery.module.css";
 import { useEffect, useState } from "react";
-import { useAccount } from "wagmi";
+import { useAccount, useChainId } from "wagmi";
 import { Network } from "alchemy-sdk";
 
-export default function MyNftsTable({
-  walletAddress,
-  collectionAddress,
-  chain,
-  pageSize,
-}: {
-  walletAddress?: string;
-  collectionAddress?: string;
-  chain?: Network;
-  pageSize?: number;
-}) {
+export default function MyNftsTable({ walletAddress }: { walletAddress?: string[] }) {
   const [nfts, setNfts] = useState();
   const [isLoading, setIsloading] = useState(false);
-  const { address, isConnected, isDisconnected } = useAccount();
+  const { isDisconnected } = useAccount();
+  const chain = useChainId();
   const [pageKey, setPageKey] = useState();
-  const [excludeFilter, setExcludeFilter] = useState(true);
   const [values, setValues] = useState({});
 
   const handleDropdownChange = (event, rowId) => {
@@ -29,60 +19,23 @@ export default function MyNftsTable({
     }));
   };
 
-  const fetchNfts = () => {
-    if (collectionAddress) {
-      getNftsForCollection();
-    } else if (walletAddress || address) {
-      getNftsForOwner();
-    }
-  };
-
-  const totalFloor = nfts && nfts.reduce((a, b) => a + (b.floor || 0), 0);
+  const totalFloor = nfts && nfts.reduce((a, b) => a + (b.floor || 0), 0).toFixed(3);
 
   const getNftsForOwner = async () => {
     setIsloading(true);
-    if (isConnected || walletAddress) {
+    if (walletAddress) {
       try {
         const res = await fetch("/api/getNftsForOwner", {
           method: "POST",
           body: JSON.stringify({
-            address: walletAddress ? walletAddress : address,
-            pageSize: pageSize,
+            addresses: walletAddress,
             chain: chain,
-            pageKey: pageKey ? pageKey : null,
-            excludeFilter: excludeFilter,
           }),
         }).then(res => res.json());
         console.log(res);
 
         setNfts(res.nfts);
-        if (res.pageKey) setPageKey(res.pageKey);
-      } catch (e) {
-        console.log(e);
-      }
-    }
-
-    setIsloading(false);
-  };
-
-  const getNftsForCollection = async () => {
-    setIsloading(true);
-
-    if (collectionAddress) {
-      try {
-        const res = await fetch("/api/getNftsForCollection", {
-          method: "POST",
-          body: JSON.stringify({
-            address: collectionAddress ? collectionAddress : address,
-            pageSize: pageSize,
-            chain: chain,
-            pageKey: pageKey ? pageKey : null,
-            excludeFilter: excludeFilter,
-          }),
-        }).then(res => res.json());
-
-        setNfts(res.nfts);
-        if (res.pageKey) setPageKey(res.pageKey);
+        // if (res.pageKey) setPageKey(res.pageKey);
       } catch (e) {
         console.log(e);
       }
@@ -92,8 +45,8 @@ export default function MyNftsTable({
   };
 
   useEffect(() => {
-    fetchNfts();
-  }, []);
+    getNftsForOwner();
+  }, [walletAddress]);
 
   if (isDisconnected) return <p>Loading...</p>;
   return (
@@ -101,9 +54,9 @@ export default function MyNftsTable({
       <div className="">
         <div className="stats shadow">
           <div className="stat">
-            <div className="stat-title">My wallet's Total Value</div>
+            <div className="stat-title">My wallet's total value</div>
             <div className="stat-value">{totalFloor} ETH</div>
-            <div className="stat-desc">Address: {walletAddress}</div>
+            {/* <div className="stat-desc">Address: {walletAddress}</div> */}
           </div>
         </div>
         <div>
@@ -175,7 +128,7 @@ export default function MyNftsTable({
           </table>
         </div>
       </div>
-      {pageKey && (
+      {/* {pageKey && (
         <div className={styles.button_container}>
           <a
             className={styles.button_black}
@@ -186,49 +139,7 @@ export default function MyNftsTable({
             Load more
           </a>
         </div>
-      )}
-    </div>
-  );
-}
-
-function NftCard({ nft }) {
-  return (
-    <div className={styles.card_container}>
-      <div className={styles.image_container}>
-        <img src={nft.media}></img>
-      </div>
-      <div className={styles.info_container}>
-        <div className={styles.title_container}>
-          <h3>{nft.title.length > 20 ? `${nft.title.substring(0, 15)}...` : nft.title}</h3>
-          <p>Floor: {nft.floor}</p>
-        </div>
-        <hr className={styles.separator} />
-        <div className={styles.symbol_contract_container}>
-          <div className={styles.symbol_container}>
-            <p>
-              {nft.collectionName && nft.collectionName.length > 20
-                ? `${nft.collectionName.substring(0, 20)}`
-                : nft.collectionName}
-            </p>
-
-            {nft.verified == "verified" ? (
-              <img
-                src={
-                  "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Twitter_Verified_Badge.svg/2048px-Twitter_Verified_Badge.svg.png"
-                }
-                width="20px"
-                height="20px"
-              />
-            ) : null}
-          </div>
-          <div className={styles.contract_container}>
-            {nft.contract.slice(0, 6)}...{nft.contract.slice(38)}
-            {/* <img src={"https://etherscan.io/images/brandassets/etherscan-logo-circle.svg"} width="15px" height="15px" /> */}
-          </div>
-        </div>
-
-        <div className={styles.description_container}>{nft.description}</div>
-      </div>
+      )} */}
     </div>
   );
 }
--- packages/nextjs/pages/my-nfts.tsx
+++ packages/nextjs/pages/my-nfts.tsx
@@ -1,30 +1,29 @@
 import { Network } from "alchemy-sdk";
 import type { NextPage } from "next";
 import Head from "next/head";
-import { useAccount } from "wagmi";
-import NftGallery from "~~/components/alchemy/NftGallery";
+import { useAccount, useNetwork } from "wagmi";
 import MyNftsTable from "~~/components/alchemy/MyNftsTable";
 
 import { ContractData, ContractInteraction } from "~~/components/ExampleUi";
 
 const ExampleUI: NextPage = () => {
-  const { address } = { address: "0xCe90a7949bb78892F159F428D0dC23a8E3584d75" }; //useAccount();
+  const { address } = useAccount();
 
   return (
     <>
-      <Head>
+      {/* <Head>
         <title>Scaffold-eth Example Ui</title>
-      </Head>
-      <div className="grid lg:grid-cols-2 flex-grow" data-theme="exampleUi">
+      </Head> */}
+
+      {/* <div className="grid lg:grid-cols-2 flex-grow" data-theme="exampleUi">
         TODO: NFTs with bids, order desc by offer value
         <br />
         TODO: NFTs I own, in vault, borrowed against at less than 100%, order desc by borrowed amount
         <br />
         TODO: NFTs in my wallet, order desc by maket value
-      </div>
-      {/* {address} */}
-      <MyNftsTable walletAddress={address} chain={Network.ETH_MAINNET} />
+      </div> */}
 
+      {address && <MyNftsTable walletAddress={[address]} />}
       {/* <NftGallery walletAddress={address} chain={Network.ETH_MAINNET} /> */}
     </>
   );
